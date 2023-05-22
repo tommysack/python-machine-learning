fd3e4b56839d30b2aa8134fd02f147ecd763ae0a
Tommaso Sacramone <tommasosacramone.www@gmail.com>
2023-05-22 05:30:14 +0200

    only comment

--- src/supervised-learning/regression/linear-regression-ols.py
+++ src/supervised-learning/regression/linear-regression-ols.py
@@ -14,6 +14,12 @@ diabets = load_diabetes(as_frame=True)
 #General info
 print(diabets.DESCR)
 
+'''
+The data are points in an hyperspace of 11 dimensions.
+The objective is to find the best hyperplane, then the "best" linear function f, in hyperspace.
+In this case we use LinearRegression that use a Closed-Form solution trying to minimize the sum of squared residuals OLS.
+'''
+
 #Separates data in Dataframe/Series columns data/target 
 X = diabets.data 
 Y = diabets.target 
@@ -50,7 +56,7 @@ print("\nX max after scaling")
 print("Max train", np.amax(X_train))
 print("Max test", np.amax(X_test))
 
-linear_regression = LinearRegression() #LinearRegression uses Closed-Form Solution/OLS
+linear_regression = LinearRegression() #LinearRegression uses Closed-Form/OLS
 linear_regression.fit(X_train, Y_train) #Training the model
 Y_test_predicted = linear_regression.predict(X_test) #Predict Y_test from X_test
 
