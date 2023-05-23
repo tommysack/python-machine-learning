5e455cf787fd78ac13903816a142817b70647cd9
Tommaso Sacramone <tommasosacramone.www@gmail.com>
2023-05-23 15:51:21 +0200

    polynomial regression

--- src/supervised-learning/regression/linear-regression-ols.py
+++ src/supervised-learning/regression/linear-regression-ols.py
@@ -16,7 +16,7 @@ print(diabets.DESCR)
 
 '''
 The data are points in an hyperspace of 11 dimensions.
-The objective is to find the best hyperplane, then the "best" linear function f, in hyperspace.
+The objective is to find the best hyperplane, then the "best" linear function f (weights and biases), in hyperspace.
 In this case we use LinearRegression that use a Closed-Form solution trying to minimize the sum of squared residuals OLS.
 '''
 
@@ -57,7 +57,7 @@ print("Max train", np.amax(X_train))
 print("Max test", np.amax(X_test))
 
 linear_regression = LinearRegression() #LinearRegression uses Closed-Form/OLS
-linear_regression.fit(X_train, Y_train) #Training the model
+linear_regression.fit(X_train, Y_train) #Building the model
 Y_test_predicted = linear_regression.predict(X_test) #Predict Y_test from X_test
 
 #Model evaluation
