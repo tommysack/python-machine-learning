26a985424ca9592faa7cbc356fbfb50e28304c34
Tommaso Sacramone <tommasosacramone.www@gmail.com>
2023-05-31 15:25:20 +0200

    bernoulli

--- src/supervised-learning/classification/linear/binary/bernoulli-nb.py
+++ src/supervised-learning/classification/linear/binary/bernoulli-nb.py
@@ -1,11 +1,8 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.naive_bayes import BernoulliNB
-from sklearn.metrics import accuracy_score
 
 #Load data
 reviews_df = pd.read_csv("../../../../../data/movie_review.csv")
@@ -24,7 +21,7 @@ reviews_df.isnull().sum()
 '''
 The data are phrases in natural language.
 The goal is to assign a class label Y (binary classification with values "pos" or "neg") to input X.
-In this case we use BernoulliNB that use  Naive Bayes algorithm.
+In this case we use BernoulliNB that use Naive Bayes algorithm.
 It makes a binary classifier. 
 '''
 
@@ -32,13 +29,14 @@ It makes a binary classifier.
 X = reviews_df["text"].values 
 Y = reviews_df["tag"].values
 
-#BernoulliNB is designed for binary/boolean features, than encoding of X with CountVectorizer
-vectorizer = CountVectorizer(stop_words='english', lowercase=True)
-X = vectorizer.fit_transform(X)
+#BernoulliNB is designed for binary/boolean features, than we need one-hot encoding. 
+#We can encode X with "bag of words" CountVectorizer using binary=True parameter (One-hot).
+vectorizer = CountVectorizer(binary=True, stop_words='english', lowercase=True)
+X = vectorizer.fit_transform(X) #it returns scipy.sparse._csr.csr_matrix that is allowed for train_test_split 
 #print("Features names after vectorizer: ", vectorizer.get_feature_names_out())
 
 #Separates data in rows train/test
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.3, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 bernoulli = BernoulliNB()
 bernoulli.fit(X_train, Y_train) #Building model
@@ -53,6 +51,32 @@ print("SCORE: ", bernoulli.score(X_test, Y_test)) #Best possible score is 1.0
 
 '''
 The metric makes me think that there is moderate overfitting.
+Try GridSearchCV to optimize parameters and to use k-fold cross validation.
+'''
+
+grid_search_cv = GridSearchCV(
+  estimator=BernoulliNB(), 
+  param_grid={
+  "alpha": [0.0001, 1, 2, 10, 100]
+  }, 
+  cv=10
+) 
+grid_search_cv.fit(X_train, Y_train)
+
+bernoulli_grid = BernoulliNB(**grid_search_cv.best_params_)
+bernoulli_grid.fit(X_train, Y_train)
+
+#Model overfitting evaluation (the percentage of samples that were correctly classified)
+print("\nModel overfitting evaluation")
+print("SCORE: ", bernoulli_grid.score(X_train, Y_train)) #Best possible score is 1.0
+
+#Model evaluation (the percentage of samples that were correctly classified)
+print("\nModel evaluation")
+print("SCORE: ", bernoulli_grid.score(X_test, Y_test)) #Best possible score is 1.0
+
+'''
+Also with GridSearchCV the situation has not improved:
+the metric makes me think that again there is moderate overfitting.
 '''
 
 #Try to predict a new case
@@ -62,6 +86,5 @@ x = vectorizer.transform(x)
 
 y = bernoulli.predict(x)
 print("\nSentiment analysis of review: ", y[0])
-
 
 
