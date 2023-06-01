3a04f22c0b6a5a7e36cafcedba8164b4e9ab6991
Tommaso Sacramone <tommasosacramone.www@gmail.com>
2023-06-01 12:33:30 +0200

    multinomial

--- src/supervised-learning/classification/linear/binary/bernoulli-nb.py
+++ src/supervised-learning/classification/linear/binary/bernoulli-nb.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import seaborn as sns
+from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 
 #Load data
@@ -29,54 +30,36 @@ It makes a binary classifier.
 X = reviews_df["text"].values 
 Y = reviews_df["tag"].values
 
-#BernoulliNB is designed for binary/boolean features, than we need one-hot encoding. 
-#We can encode X with "bag of words" CountVectorizer using binary=True parameter (One-hot).
-vectorizer = CountVectorizer(binary=True, stop_words='english', lowercase=True)
-X = vectorizer.fit_transform(X) #it returns scipy.sparse._csr.csr_matrix that is allowed for train_test_split 
-#print("Features names after vectorizer: ", vectorizer.get_feature_names_out())
-
 #Separates data in rows train/test
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 bernoulli = BernoulliNB()
-bernoulli.fit(X_train, Y_train) #Building model
 
-#Model overfitting evaluation (the percentage of samples that were correctly classified)
+#BernoulliNB is designed for binary/boolean features, than we need one-hot encoding. 
+#We can encode X with "bag of words" CountVectorizer using binary=True parameter (One-hot).
+#It returns scipy.sparse._csr.csr_matrix that is allowed for train_test_split 
+vectorizer = CountVectorizer(binary=True, stop_words='english', lowercase=True)
+
+X_train_vector = vectorizer.fit_transform(X_train) 
+
+bernoulli.fit(X_train_vector, Y_train) #Building model
+
+Y_train_predicted = bernoulli.predict(X_train_vector)
+
+#Model overfitting evaluation (the Harmonic Precision-Recall Mean)
 print("\nModel overfitting evaluation")
-print("SCORE: ", bernoulli.score(X_train, Y_train)) #Best possible score is 1.0
+print("F1 SCORE: ", metrics.f1_score(Y_train, Y_train_predicted, average='macro')) #Best possible score is 1.0
 
-#Model evaluation (the percentage of samples that were correctly classified)
+X_test_vector = vectorizer.transform(X_test)
+
+Y_test_predicted = bernoulli.predict(X_test_vector)
+
+#Model evaluation (the Harmonic Precision-Recall Mean)
 print("\nModel evaluation")
-print("SCORE: ", bernoulli.score(X_test, Y_test)) #Best possible score is 1.0
+print("F1 SCORE: ", metrics.f1_score(Y_test, Y_test_predicted, average='macro')) #Best possible score is 1.0
 
 '''
 The metric makes me think that there is moderate overfitting.
-Try GridSearchCV to optimize parameters and to use k-fold cross validation.
-'''
-
-grid_search_cv = GridSearchCV(
-  estimator=BernoulliNB(), 
-  param_grid={
-  "alpha": [0.0001, 1, 2, 10, 100]
-  }, 
-  cv=10
-) 
-grid_search_cv.fit(X_train, Y_train)
-
-bernoulli_grid = BernoulliNB(**grid_search_cv.best_params_)
-bernoulli_grid.fit(X_train, Y_train)
-
-#Model overfitting evaluation (the percentage of samples that were correctly classified)
-print("\nModel overfitting evaluation")
-print("SCORE: ", bernoulli_grid.score(X_train, Y_train)) #Best possible score is 1.0
-
-#Model evaluation (the percentage of samples that were correctly classified)
-print("\nModel evaluation")
-print("SCORE: ", bernoulli_grid.score(X_test, Y_test)) #Best possible score is 1.0
-
-'''
-Also with GridSearchCV the situation has not improved:
-the metric makes me think that again there is moderate overfitting.
 '''
 
 #Try to predict a new case
---
+++ src/supervised-learning/classification/linear/multiclass/multinomial-nb.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+#Load data
+news = fetch_20newsgroups(random_state=42)
+
+#General info
+print(news.DESCR) #20 newsgroups dataset (18000 newsgroups)
+news.target_names #All categories labels
+news.data[10] #Content of article with index 10
+news.target[10] #Category of article with index 10
+
+'''
+The data are articles in natural language.
+The goal is to assign a class label Y (multi-class classification with values news_train.target_names) to input X.
+In this case we use MultinomialNB that use Naive Bayes algorithm.
+It makes a multi-class classifier. 
+'''
+
+#Separates data in rows train/test
+news_train = fetch_20newsgroups(subset="train")
+X_train = news_train.data 
+Y_train = news_train.target
+news_test = fetch_20newsgroups(subset="test")
+X_test = news_test.data 
+Y_test = news_test.target
+
+multinomial = MultinomialNB()
+
+#MultinomialNB is designed for integer feature counts, than we need encoding. 
+#We can encode X with "bag of words" CountVectorizer.
+
+#To transforms the matrix to tf-idf representation we can encode with TfidfTransformer. 
+#It rankes a word in a doc, based on the frequency that the word appears in all docs.
+#The words that appears once have higher score than those that appears most frequently.
+
+#TfidfTransformer = CountVectorizer + TfidfTransformer
+vectorizer = TfidfVectorizer()
+
+X_train_vector = vectorizer.fit_transform(X_train) 
+
+multinomial.fit(X_train_vector, Y_train)
+
+Y_train_predicted = multinomial.predict(X_train_vector)
+
+#Model overfitting evaluation (the Harmonic Precision-Recall Mean)
+print("\nModel overfitting evaluation")
+print("F1 SCORE: ", metrics.f1_score(Y_train, Y_train_predicted, average='macro')) #Best possible score is 1.0
+
+X_test_vector = vectorizer.transform(X_test)
+
+Y_test_predicted = multinomial.predict(X_test_vector)
+
+#Model evaluation (the Harmonic Precision-Recall Mean)
+print("\nModel evaluation")
+print("F1 SCORE: ", metrics.f1_score(Y_test, Y_test_predicted, average='macro')) #Best possible score is 1.0
+
+'''
+The metric makes me think that there is moderate overfitting.
+'''
+
+#Try to predict a new case
+x = ["The Italy will have to vote for the political elections"]
+x_vector = vectorizer.transform(x)
+y = multinomial.predict(x_vector)
+print("\nCategory of doc: ", news.target_names[y[0]])
+    
+
